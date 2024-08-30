0ce0ae7877e90e82dd1c165e8b8c29b01d9d99e0
MatWaze <coolmartun@gmail.com>
2024-08-30 20:29:50 +0400

    Initialized values to 0.

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,9 +1,20 @@
 #include "Account.hpp"
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
 	static int i = 0;
 	this->_amount = initial_deposit;
+	this->_totalAmount += this->_amount;
 	this->_accountIndex = i++;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	this->_amount = deposit;
+	this->_totalAmount += this->_amount;
 }
