770abd15d707c1da834bdd4fffa9166c6e89c39c
MatWaze <coolmartun@gmail.com>
2024-09-02 15:42:30 +0400

    Completed.

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -2,15 +2,46 @@
 
 void Harl::complain(std::string level)
 {
-    std::string array[4] = {"debug", "info", "warning", "error"};
+    std::string array[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
+    int j = 0;
     for (int i = 0; i < 4; i++)
     {
         if (array[i] == level)
         {
-            (this->*_memberPtrs[i])();
+            j = i;
             break;
         }
+    }
+    std::cout << "index: " << j << std::endl;
+    switch (j)
+    {
+        case 0:
+            for (int i = 0; i < 4; i++)
+            {
+                std::cout << "[ " << array[i] << " ]" << std::endl;
+                (this->*_memberPtrs[i])();
+            }
+            break;
+        case 1:
+            for (int i = 1; i < 4; i++)
+            {
+                std::cout << "[ " << array[i] << " ]" << std::endl;
+                (this->*_memberPtrs[i])();
+            }
+            break;
+        case 2:
+            for (int i = 2; i < 4; i++)
+            {
+                std::cout << "[ " << array[i] << " ]" << std::endl;
+                (this->*_memberPtrs[i])();
+            }
+            break;
+        case 3:
+            std::cout << "here" << std::endl;
+            std::cout << "[ " << "ERROR" << " ]" << std::endl;
+            (this->*_memberPtrs[3])();
+            break;
     }
 }
 
